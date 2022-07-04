a1457ed5b7dbf7adb102eb3a7ed182900e220f47
Fernando Hidalgo <fernan.hidalgo02@gmail.com>
2022-07-04 14:58:41 +0200

    Back config and some Categoria endpoints

---
+++ Sistema/Sistema.Web/Models/Almacen/Categoria/ActualizarViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sistema.Web.Models.Almacen.Categoria
+{
+    public class ActualizarViewModel
+    {
+        [Required]
+        public int idcategoria { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre debe de tener entre 3 y 50 caracteres")]
+        public string nombre { get; set; }
+        [StringLength(256)]
+        public string descripcion { get; set; }
+    }
+}
